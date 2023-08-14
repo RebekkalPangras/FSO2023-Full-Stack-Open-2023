1668f16516d7806eb2ddcf2073bdcd524fd12870
Rebekkal <rebeccapancras@gmail.com>
2023-08-13 18:33:45 -0600

    Move the statistics part inside a Statistics component ex-1.8

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -15,14 +15,14 @@ const App = () => {
             <Button handleClick={increaseGood} text="good" />
             <Button handleClick={increaseNeutral} text="neutral" />
             <Button handleClick={increaseBad} text="bad" />
-            <Display good={good} neutral={neutral} bad={bad} />
+            <Statistics good={good} neutral={neutral} bad={bad} />
         </div>
     )
 }
 
 const Button = ({ handleClick, text }) => (<button onClick={handleClick}>{text}</button>)
 
-const Display = ({ good, neutral, bad }) => {
+const Statistics = ({ good, neutral, bad }) => {
     const total = good + neutral + bad
     const average = (good * 1 + neutral * 0 + bad * -1) / total
     const pos_percent = good / total;
