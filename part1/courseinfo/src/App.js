1c92160d965d5e66e3db391bc89c809060a7f94f
Rebekkal <rebeccapancras@gmail.com>
2023-08-17 21:32:41 -0600

    Declare separate Course component ex-2.5

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,39 +1,4 @@
-const Course = ({ course }) => {
-  return (
-    <div>
-      <Header name={course.name} />
-      <Content parts={course.parts} />
-      <Total parts={course.parts} />
-    </div>
-  );
-}
-
-const Header = ({ name }) => {
-  return (
-    <h1>{name}</h1>
-  )
-}
-
-const Part = ({ name, exercises }) => {
-  return (
-    <p>{name} {exercises}</p>
-  )
-}
-
-const Content = ({ parts }) => {
-  return (
-    <div>
-      {parts.map(part => <Part key={part.id} name={part.name} exercises={part.exercises} />)}
-    </div>
-  )
-}
-
-const Total = ({ parts }) => {
-  var total = parts.reduce((sum, part) => sum + part.exercises, 0)
-  return (
-    <h4>Total of {total} exercises</h4>
-  )
-}
+import Course from "./Course";
 
 const App = () => {
   const courses = [
