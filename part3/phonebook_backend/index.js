a68f4435b4d85a537cfe2a0d3aacd306f25c21c3
Rebekkal <rebeccapancras@gmail.com>
2023-10-01 17:59:06 -0600

    Replaced custom token to morgan middleware ex-3.8

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -4,7 +4,9 @@ const app = express()
 const morgan = require('morgan')
 
 app.use(express.json())
-app.use(morgan('tiny'))
+app.use(morgan(':method :url :status :req[content-length] :response-time ms :req-body'));
+
+morgan.token('req-body', req => JSON.stringify(req.body))
 
 let persons = [
     {
