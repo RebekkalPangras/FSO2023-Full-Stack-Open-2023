01254f7bc675ac3807c58fd5e49c61b1b12ae415
Rebekkal <rebeccapancras@gmail.com>
2023-10-22 14:44:41 -0600

    Modularizing the node project ex-4.2

--- part4/bloglist/index.js
+++ part4/bloglist/index.js
@@ -2,9 +2,9 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const mongoose = require('mongoose')
-const Blog = require('./models/blog')
 const { MONGODB_URI, PORT } = require('./utils/config')
 const logger = require('./utils/logger')
+const blogRouter = require('./controllers/blogs')
 
 
 mongoose.set('strictQuery', false);
@@ -12,25 +12,7 @@ mongoose.connect(MONGODB_URI)
 
 app.use(cors())
 app.use(express.json())
-
-app.get('/api/blogs', (request, response) => {
-    Blog
-        .find({})
-        .then(blogs => {
-            response.json(blogs)
-        })
-})
-
-app.post('/api/blogs', (request, response) => {
-    logger.info(request.body)
-    const blog = new Blog(request.body)
-
-    blog
-        .save()
-        .then(result => {
-            response.status(201).json(result)
-        })
-})
+app.use('/api/blogs', blogRouter)
 
 app.listen(PORT, () => {
     logger.info(`Server running on port ${PORT}`)
