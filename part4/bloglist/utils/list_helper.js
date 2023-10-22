40c7a358cebad48ff57bda43e18b1746a98dba2e
Rebekkal <rebeccapancras@gmail.com>
2023-10-22 15:10:55 -0600

    Use describe and execute test cases ex-4.4

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -2,6 +2,15 @@ const dummy = (blogs) => {
     return 1
 }
 
+const totalLikes = (blogs) => {
+
+    const addLikes = (sum, blog) => {
+        return sum + blog.likes
+    }
+    return blogs.reduce(addLikes, 0)
+}
+
 module.exports = {
-    dummy
+    dummy,
+    totalLikes
 }
